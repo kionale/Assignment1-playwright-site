2c8c87e5beb459ec4fd2e97f7bb4eca565e17660
kionale <kiona.peng@gmail.com>
2023-01-28 12:58:20 -0800

    content of index and about pages added

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-This is a [Project repo](https://nextjs.org/) 
+Github link [Project repo](https://github.com/kionale/Assignment1-playwright-site) 
 
-## Getting Started
+## Requirements
 
-Media Queries
+# Data To Create
+Export an array of objects by initialising it to words that contains the information of:
+id = 0, value = Achieve Excellence
+id = 1, value = Embrace Innovation
+id = 2, value = Champion Diversity
+id = 3, value = Pursue Collaboration
+id = 4, value = Engage With Respect
+
+
+# Media Queries
 - The breaking points are as follows:
 - Desktop over 1000px
 - Tablet (ipad Air) between 600px and 1000px
@@ -10,8 +19,8 @@ Media Queries
 
 # Language use
 ```
-* JavaScript
-* HTML
-* CSS
+ JavaScript
+ HTML
+ CSS
 
 ```
---
+++ pages/about.js
@@ -0,0 +1,60 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
+
+export default function About() {
+    return (
+        <>
+            <head>
+                <title>About Us</title>
+                <meta name="author" content="MDIA 2109"></meta>
+                <meta property="og:title" content="Assignment #1 - About Us Page" />
+                <meta property="og:title" content="BCIT Digital Design and Development Diploma" />
+                <link rel="icon" href='/favicon.png' />
+            </head>
+
+            <main>
+                <header>
+                    <div><h1></h1></div>
+                    <div><h1></h1></div>
+                </header>
+                <paragraph>
+                    <div><p></p></div>
+                    <div><p></p></div>
+                    <div><p></p></div>
+                    <div><p></p></div>
+                    <div><p></p></div>
+                    <div><p></p></div>
+                    <div><p></p></div>
+                </paragraph>
+                <div><h2></h2></div>
+                <div><h2></h2></div>
+                <div><h3></h3></div>
+
+                <img src={'/icons/leftArrow.png'} />
+                <img src={'/icons/rightArrow.png'} />
+                {/* <carouselImage height="200px" background-repeat: "no-repeat"></carouselImage> */}
+
+                <div>
+                    <ul>
+                        <li>Applied & Natural Sciences</li>
+                        <li>Business & Media</li>
+                        <li>Computing & IT</li>
+                        <li>Engineering</li>
+                        <li>Health Sciences</li>
+                        <li>Trades & Apprenticeships</li>
+                    </ul>
+                </div>
+
+                <Link href='/'><img src={'icons/upwardArrow.png'} /></Link>
+                <Link href='contact'><img src={'icons/downwardArrow.png'} /></Link>
+
+
+            </main>
+
+
+        </>
+    )
+}
